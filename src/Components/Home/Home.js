feb3dba603cdafa54cede5699f5f1bf158407dbb
Yeasir-Hossain <yeasir06@gmail.com>
2022-04-05 16:34:23 +0600

    home updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,13 +6,12 @@ const Home = () => {
         <div className="mt-10 mb-10">
         <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center">
           <div className="p-5 lg:mt-10">
-            <h1 className="text-6xl font-bold px-10">HEADPHONES</h1>
+            <h1 className="text-6xl font-bold px-10">HEADPHONE REVIEW</h1>
             <h1 className="text-5xl pt-5 px-10 font-bold text-indigo-600">
               Headphones Online shop
             </h1>
-            <p className="flex justify-center text-justify px-10 pt-5">
-              You can buy headphines online with a few clicks or a convenient phone
-              call.
+            <p className="flex justify-center text-xl text-justify px-10 pt-5">
+            You can buy headphines online with a few clicks or a convenient phone call.
             </p>
             <button className="text-white mt-5 mx-10 bg-indigo-500 hover:bg-indigo-600 rounded p-3 font-bold">
               Order Now
