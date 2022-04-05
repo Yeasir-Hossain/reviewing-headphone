b2ea695a648c86ac8507cff4d111314b76583caa
Yeasir-Hossain <yeasir06@gmail.com>
2022-04-05 14:46:29 +0600

    FAQ done and charts done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Review from './Components/Review/Review';
+import Chart from './Components/Chart/Chart';
+import Faq from './Components/Faq/Faq';
+import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='reviews' element={<Review/>}/>
+        <Route path="dashboard" element={<Chart/>}/>
+        <Route path='blogs' element={<Faq/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+      <div className="font-bold text-lg text-center mt-[100px]">
+        <h2>Yeasir Hossain</h2>
+        <h3>Bangladesh Army University of Science and Technology</h3>
+        <h3>Saidpur,Nilphamari</h3>
+      </div>
+    );
+};
+
+export default About;
